b0b8acc2ca58ca9c07ee8cbc7f3eb1e2c2dac34e
AJTM <tadinaarvinjay@gmail.com>
2025-10-31 12:30:48 +0800

    Feat: finished the logic for setup_database function

--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,26 @@
-
 def setup_database():
-    pass
+    """
+    Sets up the database for the new user
+
+    It can be utilized if preferred by the user
+
+    Imports the get_connection function from database_connection module
+
+    Sets up a connection and cursor
+
+    Then opens and read the simple_python_games.sql file located in the root directory
+
+    If an error occurs, it just prints out the error
+    """
+    from utils.database_connection import get_connection
+
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursor:
+                with open('../simple_python_games.sql', 'r') as file:
+                    sql = file.read()
+                    cursor.execute(sql)
+                    connection.commit()
+    
+    except Exception as e:
+        print(f'An error has occurred: {e}')
