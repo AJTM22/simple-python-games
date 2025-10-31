dfa790fd521a04f8b8f0896de85e3b40d4cc8b23
AJTM <tadinaarvinjay@gmail.com>
2025-10-31 12:31:32 +0800

    Feat: imported the setup_database function from the database.py and left a comment for new users

--- simple_python_games.py
+++ simple_python_games.py
@@ -1,4 +1,7 @@
 from utils.login import login
+from utils.database import setup_database
 
 if __name__ == '__main__':
+    # If running the program for the first time or didn't bother creating your own database, uncomment the setup_database function to automate the creation of the database
+    # setup_database()
     login()
